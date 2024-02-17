ce549353be1e62824390c07ca69e9035e29ac76f
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-02-17 13:02:42 +0500

    Project update

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+
+//Toggle Menu
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+
+
+
