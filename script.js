cf4c4f2617cb563dac4acc489018249567c2ac35
MK Abdul Azees <115166340+mkabdulazees56@users.noreply.github.com>
2024-06-22 12:53:44 +0500

    porfolio update

--- script.js
+++ script.js
@@ -8,7 +8,21 @@ function toggleMenu() {
     icon.classList.toggle("open");
 }
 
+function showTab(tabId) {
+    const tabs = document.querySelectorAll('.project-tab');
+    tabs.forEach(tab => {
+        tab.style.display = 'none';
+    });
 
+    const tabButtons = document.querySelectorAll('.tab-button');
+    tabButtons.forEach(button => {
+        button.classList.remove('active');
+    });
 
+    document.getElementById(tabId).style.display = 'flex';
+    event.target.classList.add('active');
+}
 
+// Set the default tab to be displayed
+document.getElementById('js-projects').style.display = 'flex';
 
